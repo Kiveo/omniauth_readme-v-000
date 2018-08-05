75a40b9146de9bb180b1d36fe734168072c34c7d
Maverick Tesini <mavericktesini@gmail.com>
2018-08-05 07:03:22 +0000

    automatically backed up by learn

--- db/migrate/20180805070131_user.rb
+++ db/migrate/20180805070131_user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Migration
   def change
+    create_table :users do |t|
+      t.string :name
+      t.email_address :email
+      t.blob :image
+      t.integer :uid
+
   end
 end
